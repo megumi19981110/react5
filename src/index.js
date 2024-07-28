df5480f20f7f7d3538ad5e90dfb671987522c3bc
miyazakimegumi <coffee1110milk1998@icloud.com>
2024-07-28 16:51:38 +0900

    react3

--- src/index.js
+++ src/index.js
@@ -6,15 +6,17 @@ class Square extends React.Component {
     render() {
       return (
         <button className="square">
-          {/* TODO */}
+          {this.props.value}
         </button>
+        // 渡された値を表示
       );
     }
   }
   
   class Board extends React.Component {
     renderSquare(i) {
-      return <Square />;
+      return <Square value={i} />;
+      // valueという値をSquareに渡す
     }
   
     render() {
