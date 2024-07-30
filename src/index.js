f6e5d820dc38824d250b6c878664bff601537c22
miyazakimegumi <coffee1110milk1998@icloud.com>
2024-07-31 06:38:15 +0900

    react10

--- src/index.js
+++ src/index.js
@@ -13,48 +13,52 @@ function Square(props) {
 //9Square がクリックされると、Board から渡された onClick 関数がコールされる
 //11シンプルに書くために、React.Component を継承するクラスを定義する代わりに、props を入力として受け取り表示すべき内容を返す関数を定義
 
-    render() {
-      return (
-        <button 
-        className="square" onClick={() => this.setState({value: 'X'})}>
-          {this.state.value}
-        </button>
-        //2渡された値を表示
-        //3クリックされた場合に “X” と表示
-        //5クリックされた時に state の現在値を表示
-      );
-    }
+render() {
+  return (
+    <div>
+      <div className="board-row">
+      {this.renderSquare(0)}
+      {this.renderSquare(1)}
+      {this.renderSquare(2)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(3)}
+      {this.renderSquare(4)}
+      {this.renderSquare(5)}
+    </div>
+    <div className="board-row">
+      {this.renderSquare(6)}
+      {this.renderSquare(7)}
+      {this.renderSquare(8)}
+    </div>
+  </div>
+);
+}
+
+//2渡された値を表示
+//3クリックされた場合に “X” と表示
+//5クリックされた時に state の現在値を表示
+//ステータステキストに対応するコードは Board 内の render メソッドからは削除
+
   
   class Board extends React.Component {
     //6初期 state として 9 個のマス目に対応する 9 個の null 値をセット
     //12先手を “X” にする
 
     handleClick(i) {
-       const history = this.state.history;
-    const current = history[history.length - 1];
-    const squares = current.squares.slice();
-      if (calculateWinner(squares) || squares[i]) {
-        return;
+      renderSquare(i) {
+        return (
+          <Square
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
+          />
+        );
       }
-      squares[i] = this.state.xIsNext ? 'X' : 'O';
-      this.setState({
-        history: history.concat([{
-          squares: squares,
-        }]),
-        xIsNext: !this.state.xIsNext,
-    });
-    }
    //10マス目をクリックすると値が書き込まれるようにする
    //13X” 側と “O” 側が交互に着手できるようにする
    //17ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return する
 　　//19新しい履歴エントリを history に追加
-    
-    renderSquare(i) {
-      return 
-      <Square 
-      value={this.props.squares[i]}
-      onClick={() => this.props.onClick(i)}
-      />;
+
       //1valueという値をSquareに渡す
       //7個別の Square に現在の値（'X'、'O' または null）を伝えるようにする
       //8マス目がクリックされた時に Square にその関数を呼んでもらう
@@ -62,20 +66,9 @@ function Square(props) {
     }
   
     render() {
-      const winner = calculateWinner(this.state.squares);
-      let status;
-      if (winner) {
-        status = 'Winner: ' + winner;
-      } else {
-        status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-      }
-      //14どちらのプレーヤの手番なのかを表示
-      //16いずれかのプレーヤが勝利したかどうか判定
-      
-      render() {
-        return (
-          <div>
-            <div className="board-row">
+      return (
+        <div>
+          <div className="board-row">
             {this.renderSquare(0)}
             {this.renderSquare(1)}
             {this.renderSquare(2)}
@@ -94,8 +87,10 @@ function Square(props) {
       );
     }
   }
-  //ステータステキストに対応するコードは Board 内の render メソッドからは削除
   
+      //14どちらのプレーヤの手番なのかを表示
+      //16いずれかのプレーヤが勝利したかどうか判定
+      
   class Game extends React.Component {
     constructor(props) {
       super(props);
@@ -103,14 +98,42 @@ function Square(props) {
         history: [{
           squares: Array(9).fill(null),
         }],
+        stepNumber: 0,
         xIsNext: true,
       };
     }
     //17コンストラクタ内でセット
-    render() {
-      const history = this.state.history;
+    //21いま何手目の状態を見ているのかを表す
+    handleClick(i) {
+      const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
-      const winner = calculateWinner(current.squares);
+      const squares = current.squares.slice();
+      if (calculateWinner(squares) || squares[i]) {
+        return;
+      }
+      squares[i] = this.state.xIsNext ? "X" : "O";
+      this.setState({
+        history: history.concat([
+          {
+            squares: squares
+          }
+        ]),
+        stepNumber: history.length,
+        xIsNext: !this.state.xIsNext
+      });
+    }
+    //23「時間の巻き戻し」をしてからその時点で新しい着手を起こした場合に、そこから見て「将来」にある履歴（もはや正しくなくなったもの）を確実に捨て去ることができる
+    //24常に最後の着手後の状態をレンダーするのではなく stepNumber によって現在選択されている着手をレンダーするようにする
+
+    jumpTo(step) {
+      this.setState({
+        stepNumber: step,
+        xIsNext: (step % 2) === 0
+      });
+    }
+     //20過去の手番に「ジャンプ」するためのボタンの一覧を表示
+     //21React の key に関する警告は表示されなくなる
+     //22jumpTo メソッドを定義してその stepNumber が更新されるようにする
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -127,7 +150,7 @@ function Square(props) {
           </div>
           <div className="game-info">
           <div>{status}</div>
-            <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
           </div>
         </div>
       );
