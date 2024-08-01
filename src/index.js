f0455855f9323a14d3d4bc78a96bef44d2b4b89c
miyazakimegumi <coffee1110milk1998@icloud.com>
2024-08-02 06:58:18 +0900

    react11

--- src/index.js
+++ src/index.js
@@ -9,9 +9,9 @@ function Square(props) {
     </button>
   );
 }
-//4state を初期化
-//9Square がクリックされると、Board から渡された onClick 関数がコールされる
-//11シンプルに書くために、React.Component を継承するクラスを定義する代わりに、props を入力として受け取り表示すべき内容を返す関数を定義
+//4 state を初期化
+//9 Square がクリックされると、Board から渡された onClick 関数がコールされる
+//11 シンプルに書くために、React.Componentを継承するクラスを定義する代わりに、propsを入力として受け取り表示すべき内容を返す関数を定義
 
 render() {
   return (
@@ -35,15 +35,15 @@ render() {
 );
 }
 
-//2渡された値を表示
-//3クリックされた場合に “X” と表示
-//5クリックされた時に state の現在値を表示
-//ステータステキストに対応するコードは Board 内の render メソッドからは削除
+//2 渡された値を表示するように、{/* TODO */} を {this.props.value} に書き換え
+//3 コンソールに ‘click’ と表示されるようにする
+//5 クリックされた時にstateの現在値を表示
+//22 ステータステキストに対応するコードはBoard内のrenderメソッドからは削除
 
   
   class Board extends React.Component {
-    //6初期 state として 9 個のマス目に対応する 9 個の null 値をセット
-    //12先手を “X” にする
+    //6 初期stateとして9個のマス目に対応する9個のnull値をセット
+    //12 先手を “X” にする
 
     handleClick(i) {
       renderSquare(i) {
@@ -54,15 +54,15 @@ render() {
           />
         );
       }
-   //10マス目をクリックすると値が書き込まれるようにする
-   //13X” 側と “O” 側が交互に着手できるようにする
-   //17ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期に return する
-　　//19新しい履歴エントリを history に追加
+   //10 マス目をクリックすると値が書き込まれるようにする
+   //13 X” 側と “O” 側が交互に着手できるようにする
+   //18 ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期にreturnする
+   //20 BoardコンポーネントがsquaresとonClickプロパティをGameコンポーネントから受け取るようにする
+　　// 新しい履歴エントリをhistoryに追加
 
-      //1valueという値をSquareに渡す
-      //7個別の Square に現在の値（'X'、'O' または null）を伝えるようにする
-      //8マス目がクリックされた時に Square にその関数を呼んでもらう
-      //18Board コンポーネントが squares と onClick プロパティを Game コンポーネントから受け取るようにする
+      //1 valueという値をSquareに渡す
+      //7 個別の Square に現在の値（'X'、'O' または null）を伝えるようにする
+      //8 マス目がクリックされた時にSquareにその関数を呼んでもらう
     }
   
     render() {
@@ -88,8 +88,8 @@ render() {
     }
   }
   
-      //14どちらのプレーヤの手番なのかを表示
-      //16いずれかのプレーヤが勝利したかどうか判定
+      //15 どちらのプレーヤの手番なのかを表示
+      //16 いずれかのプレーヤが勝利したかどうか判定
       
   class Game extends React.Component {
     constructor(props) {
@@ -102,8 +102,8 @@ render() {
         xIsNext: true,
       };
     }
-    //17コンストラクタ内でセット
-    //21いま何手目の状態を見ているのかを表す
+    //19 Gameコンポーネントの初期stateをコンストラクタ内でセット
+    //27 いま何手目の状態を見ているのかを表す
     handleClick(i) {
       const history = this.state.history.slice(0, this.state.stepNumber + 1);
       const current = history[history.length - 1];
@@ -122,8 +122,9 @@ render() {
         xIsNext: !this.state.xIsNext
       });
     }
-    //23「時間の巻き戻し」をしてからその時点で新しい着手を起こした場合に、そこから見て「将来」にある履歴（もはや正しくなくなったもの）を確実に捨て去ることができる
-    //24常に最後の着手後の状態をレンダーするのではなく stepNumber によって現在選択されている着手をレンダーするようにする
+    //17 ゲームの決着が既についている場合やクリックされたマス目が既に埋まっている場合に早期にreturnするようにする
+    //21 ゲームのステータステキストの決定や表示の際に最新の履歴が使われるようにする
+    //29「時間の巻き戻し」をしてからその時点で新しい着手を起こした場合に、そこから見て「将来」にある履歴（もはや正しくなくなったもの）を確実に捨て去ることができる
 
     jumpTo(step) {
       this.setState({
@@ -131,9 +132,36 @@ render() {
         xIsNext: (step % 2) === 0
       });
     }
-     //20過去の手番に「ジャンプ」するためのボタンの一覧を表示
-     //21React の key に関する警告は表示されなくなる
-     //22jumpTo メソッドを定義してその stepNumber が更新されるようにする
+    //28 Game内にjumpToメソッドを定義してそのstepNumberが更新されるようにする
+
+     render() {
+      const history = this.state.history;
+      const current = history[this.state.stepNumber];
+      const winner = calculateWinner(current.squares);
+  
+      const moves = history.map((step, move) => {
+        const desc = move ?
+          'Go to move #' + move :
+          'Go to game start';
+        return (
+          <li key={move}>
+            <button onClick={() => this.jumpTo(move)}>{desc}</button>
+          </li>
+        );
+      });
+     //23 handleClickメソッドをBoardコンポーネントからGameコンポーネントに移動する
+     //24 Game内の handleClickメソッドで、新しい履歴エントリをhistoryに追加
+     //25 Gameのrenderメソッド内でhistoryにmapを作用させる
+     //26 Reactのkeyに関する警告は表示されなくなる
+     //30 常に最後の着手後の状態をレンダーするのではなく stepNumber によって現在選択されている着手をレンダーするようにする
+
+      let status;
+      if (winner) {
+        status = "Winner: " + winner;
+      } else {
+        status = "Next player: " + (this.state.xIsNext ? "X" : "O");
+      }
+
       let status;
       if (winner) {
         status = 'Winner: ' + winner;
@@ -155,8 +183,7 @@ render() {
         </div>
       );
     }
-  }
-  //ゲームのステータステキストの決定や表示の際に最新の履歴が使われるようにする
+  //14 どちらのプレーヤの手番なのかを表示
   
   // ========================================
   
@@ -183,4 +210,4 @@ render() {
     return null;
   }
   
-  //15ゲームが決着して次の手番がなくなった時にそれを表示する
+  //16 ゲームが決着して次の手番がなくなった時にそれを表示する
